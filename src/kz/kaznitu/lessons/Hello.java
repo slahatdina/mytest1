34a3a1fb6923493256b7bee79aac0160bac45eb3
你的名称 <email>
2017-10-19 16:39:47 +0600

    өзгерді

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,7 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main(String args[]){
             System.out.println("Hello world!");
+        System.out.println("слахат");
+        System.out.println("дина");
     }
 }
